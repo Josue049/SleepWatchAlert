a22a18d8e9e5f01c925389040871b576208c7e31
Josué Gutiérrez <josue09guti@gmail.com>
2024-01-05 03:24:22 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# SLEEPWATCH ALERT
+
+#### Description:
+
+Through this project, I seek to create a free alternative to baby video surveillance cameras. This was done thinking about families who do not have the financial resources to purchase a specialized camera, but who have a computer and/or cell phone at home. The goal is to provide easy and free access so that parents can monitor and ensure the well-being of their baby. Additionally, the project is designed to be lightweight and run smoothly on low-resource computers.
+
+![Screenshot0](https://i.ibb.co/jLNTdhg/Screenshot.png)
+![Screenshot1](https://i.ibb.co/X3sSc80/Screenshot-1.png)
+
+At the moment the project is being planned so that a person can download it and run it on their computer locally.
+
+It is a project with a lot of growth potential and in which I have put a lot of effort these days.
+
+
+### USE
+The project consists of creating a web page that the mother or father can access, activate the camera and, through artificial intelligence, detect the baby's eyes to verify if she is awake or still sleeping. If the baby wakes up, she will automatically send an SMS to the mother's phone, although ideally the mother will have a smart watch to feel the vibration on her arm when the SMS arrives.
+
+### INSPIRATION FROM A REAL CASE
+In my house we constantly make video calls to monitor my niece and notify her mother if she wakes up, and although it seems like a good solution, this can be subject to several inconveniences such as always having to ask someone else for help or simply having whoever is watching get distracted
+
+### EYE DETECTION
+The objective is to detect if the baby has its eyes open or closed. My first go was to use OpenCV with a Haarcascade for eye detection. Sometimes it worked well, but it also had a problem with false positives. Which could be problematic in a home environment where eyes could be detected on the baby's blanket or polo shirt. So after some searching I found a perfect solution: MediaPipe Face Mesh. Basically, it provides a mesh that covers the face, and by only using the points that form the eyes, we exponentially reduce errors, to the point that I haven't had any problems so far. To go deeper into the topic I found this page: [https://omes-va.com/contador-de-parpadeos-python-mediapipe-facemesh-opencv/](https://omes-va.com/contador -de -blinks-python-mediapipe-facemesh-opencv/) (Credits to the authors). This page talks about an article that explains an equation for measuring the aspect ratio between the eyes, as well as providing an example of practical use. The full article is here: [https://vision.fe.uni-lj.si/cvww2016/proceedings/papers/05.pdf](https://vision.fe.uni-lj.si/cvww2016/proceedings/ articles/05.pdf). Thanks to these resources I was able to carry out this project. I implemented the equation mentioned in the article, performed frame-by-frame recognition in real time, marking "ASLEEP" or "AWAKE" on the console. And finally I eliminated the repetitions so that it only shows when going from one state to another.
+
+### SEND AN SMS MESSAGE
+The Twilio API is used with which SMS can be sent. Therefore, it is necessary for the user to create an account and fill out the necessary fields on the page to be able to use this service. Use of the API is not sponsored in any way and is only used for its efficiency.
+
+### PUT IT ON A WEB PAGE
+This was THE BIGGEST PROBLEM without a doubt and the most time consuming. I had to change my approach to the problem at least 5 times (if I don't forget any).
+
+1. First, I had the idea to turn on the camera on the client, send it to the server and from the server send the image by applying eye recognition to the client. But this was extremely slow and, due to the nature of the project, it had to be as fast as possible so that the signal reached the mother practically instantly.
+
+2. The next idea was to move the entire project to JavaScript with OpenCV.js and run detection on the client side. However, this would mean starting over from scratch, and OpenCV.js also had a bug that said you had to recompile OpenCV.js by changing a default configuration.
+
+3. It occurred to me that if we removed the highlighting of the eyes so that they are not shown to the client, we could drastically reduce the wait times (so that the eye detection would be done on the server side and not reflected so that the client saw it). I tried running the program from my PC, but it was too slow. Despite everything, I kept the idea of ​​only doing eye detection on the server.
+
+4. At some point, I even thought about doing a computer project, but since it was not practical for mother's use, I quickly discarded it.
+
+5. It occurred to me that the frames were literally images or, rather, an image. The idea is simple: the camera turns on on the client side, takes the current image and sends it to the server every second (in my experience, doing it faster causes problems). The server receives the image "new_frame.jpg" and, with a loop, updates the image to the current frame, causing a single image "new_frame.jpg" to constantly change its content and constantly perform eye detection on it. It sounds confusing, but it works very well and is almost immediate. This idea was the definitive one as it worked and did not consume many resources with the creation of new images.
+
+### LOCAL USE OF THE PROGRAM
+I recommend using HTTPS, since activating the camera on the client side is not allowed when using HTTP. The best practice would be to use the program from a local network so that it can be accessed from a cell phone within the same house. To achieve this, HTTPS can be implemented with OPENSSL.
+
+A certificate must be generated with OPENSSL and then copy the "server.cer" and "server.key" files to the "cer" folder.
+
+
+## And finally, the sum of all this effort led to the creation of SleepWatch ALERT 2.0
+The operation is simple: Turn on the server, enter the page, create an account, log in and READY! You can now focus on your baby and wait for an alert on your smart watch or cell phone by default.
+
+![Screenshot2](https://i.ibb.co/NVcsPm6/Screenshot-2.png)
+
+I originally created this project as my final project for Harvard CS50 in December 2023, but I refined it for release in competition in 2024. So it's a version 2.0 in spirit, but the first to be made public.
---
+++ app.py
@@ -0,0 +1,216 @@
+import os
+from functools import wraps
+import base64
+from PIL import Image
+from io import BytesIO
+from cs50 import SQL
+from flask import (Flask, redirect, render_template, request, session, jsonify, make_response)
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+import cv2
+import mediapipe as mp
+import numpy as np
+from twilio.rest import Client
+
+mp_face_mesh = mp.solutions.face_mesh
+index_left_eye = [33, 160, 158, 133, 153, 144]
+index_right_eye = [362, 385, 387, 263, 373, 380]
+EAR_THRESH = 0.20
+NUM_FRAMES = 10
+Frames_Dormido = 0
+OJOS1 = ""
+OJOS2 = ""
+i = 0
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = SQL("sqlite:///registros.db")
+
+def apology(message, code=400):
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+        ("%", "~p"), ("#", "~h"), ("/", "~s"), ('\"', "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.route("/")
+@login_required
+def index():
+    try:
+        base = db.execute("SELECT user_baby, phone_number FROM users WHERE id = (?)", session["user_id"])
+        number = base[0]["phone_number"]
+    except Exception as e:
+        session.clear()
+        return render_template("login.html")
+
+    return render_template("index.html", number=number)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return apology("Must provide username", 403)
+
+        elif not request.form.get("password"):
+            return apology("Must provide password", 403)
+
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+        print(rows)
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 403)
+        session["user_id"] = rows[0]["id"]
+        return redirect("/")
+    else:
+        return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        name = request.form.get("username")
+        name_baby = request.form.get("name_baby")
+        phone_number = request.form.get("phone_number")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        account = request.form.get("account")
+        token = request.form.get("token")
+        twilio_number = request.form.get("twilio_number")
+
+        if name == "" or name_baby == "" or phone_number == "" or password == "" or confirmation == "" or account == "" or token == "" or twilio_number == "":
+            return apology("Complete your information", 400)
+        
+        hashed_password = generate_password_hash(password, method="pbkdf2:sha256")
+        password_matched = check_password_hash(hashed_password, confirmation)
+        if password_matched:
+            try:
+                db.execute(
+                    "INSERT INTO users (username, user_baby, hash, phone_number, twilio_number, account, token) VALUES ((?),(?),(?),(?),(?),(?),(?))", name, name_baby, hashed_password, phone_number, twilio_number, account, token)
+                return render_template("login.html")
+            except Exception as e:
+                return make_response(
+                    render_template("register.html", mostrar_alert=True), 400
+                )
+        else:
+            return apology("Different passwords", 400)
+        
+
+    return render_template("register.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+    try:
+        os.remove(str(session["user_id"])+'nuevo_frame.jpg')
+    except Exception as e:
+        print("Unstored image")
+    session.clear()
+    return redirect("/")
+
+@app.route('/get_frame', methods=['POST'])
+def get_frame():
+    global mp_face_mesh, index_left_eye, index_right_eye, EAR_THRESH, NUM_FRAMES, Frames_Dormido, OJOS1, OJOS2, i
+
+    base = db.execute("SELECT user_baby, phone_number, twilio_number, account, token FROM users WHERE id = (?)", session["user_id"])
+    number = base[0]["phone_number"]
+    number = "+"+str(number)
+    babyname = base[0]["user_baby"]
+    babyname = "HAS WOKEN UP " + babyname
+    account = base[0]["account"]
+    token = base[0]["token"]
+    twilio_number = base[0]["twilio_number"]
+    twilio_number = "+"+str(twilio_number)
+
+    client = Client(account, token)
+
+    def eye_aspect_ratio(coordinates):
+        A = np.linalg.norm(np.array(coordinates[1]) - np.array(coordinates[5]))
+        B = np.linalg.norm(np.array(coordinates[2]) - np.array(coordinates[4]))
+        C = np.linalg.norm(np.array(coordinates[0]) - np.array(coordinates[3]))
+        return (A + B) / (2 * C)
+
+    frame_data = request.form.get('frame')
+    binary_data = base64.b64decode(frame_data.split(',')[1])
+    new = Image.open(BytesIO(binary_data))
+    new.save(str(session["user_id"])+'nuevo_frame.jpg')
+    cap = cv2.VideoCapture(str(session["user_id"])+'nuevo_frame.jpg')
+
+    with mp_face_mesh.FaceMesh(static_image_mode=False, max_num_faces=1) as face_mesh:
+
+        while True:
+            ret, frame = cap.read()
+            if ret == False:
+                break
+            frame = cv2.flip(frame, 1)
+            height, width, _ = frame.shape
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            res = face_mesh.process(frame_rgb)
+
+            coordinates_left_eye = []
+            coordinates_right_eye = []
+
+            if res.multi_face_landmarks is not None:
+                for face in res.multi_face_landmarks:
+                    for index in index_left_eye:
+                            x = int(face.landmark[index].x * width)
+                            y = int(face.landmark[index].y * height)
+                            coordinates_left_eye.append([x, y])
+                            cv2.circle(frame, (x, y), 2, (0, 255, 255), 1)
+                            cv2.circle(frame, (x, y), 1, (128, 0, 250), 1)
+                    for index in index_right_eye:
+                            x = int(face.landmark[index].x * width)
+                            y = int(face.landmark[index].y * height)
+                            coordinates_right_eye.append([x, y])
+                            cv2.circle(frame, (x, y), 2, (128, 0, 250), 1)
+                            cv2.circle(frame, (x, y), 1, (0, 255, 255), 1)
+
+                left = eye_aspect_ratio(coordinates_left_eye)
+                right = eye_aspect_ratio(coordinates_right_eye)
+                ear = (left + right)/2
+                
+                if ear < EAR_THRESH:
+                    OJOS2 = OJOS1
+                    Frames_Dormido += 1
+                    if Frames_Dormido > NUM_FRAMES:
+                        OJOS1 = "ASLEEP"
+                        Frames_Dormido = 0
+                elif ear > EAR_THRESH:
+                    OJOS2 = OJOS1
+                    OJOS1 = "AWAKE"
+                else:
+                    OJOS1="EYES NOT FOUND"
+                if OJOS1 != OJOS2:
+                    print(ear)
+                    print(OJOS1)
+                    if OJOS1 == "AWAKE" and i != 0:
+                        client.messages.create(to=number, from_=twilio_number, body=babyname)
+
+                i +=1
+    cap.release()
+
+    return jsonify({"message": "Frame received successfully"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, ssl_context=("cert/server.cer", "cert/server.key"))
